a7e60a2cc3358a7c9669700b278a643432aa4fa6
Karine66 <shon@live.fr>
2021-08-17 14:58:37 +0200

    set up microservices

--- src/main/java/com/mycompany/invoise/core/controller/InvoiceControllerInterface.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.invoise.core.controller;
-
-import com.mycompany.invoise.core.entity.Invoice;
-import com.mycompany.invoise.core.controller.service.InvoiceServiceInterface;
-
-public interface InvoiceControllerInterface {
-   String createInvoice(Invoice invoice);
-    void setInvoiceService(InvoiceServiceInterface invoiceService);
-}
